aaca482d9f229c511532aa27e9a10ef9e0ed1b0f
Todd Rinaldo <toddr@cpan.org>
2010-03-30 17:56:45 +0000

    bump version and add author
    git-svn-id: http://svn.ali.as/cpan/trunk/IPC-Run@11799 88f4d9cd-8a04-0410-9d60-8f63309c3137

--- lib/IPC/Run/Win32Helper.pm
+++ lib/IPC/Run/Win32Helper.pm
@@ -24,7 +24,7 @@ use Carp;
 use IO::Handle;
 use vars qw{ $VERSION @ISA @EXPORT };
 BEGIN {
-	$VERSION = '0.87';
+	$VERSION = '0.88';
 	@ISA = qw( Exporter );
 	@EXPORT = qw(
 		win32_spawn
--- lib/IPC/Run/Win32IO.pm
+++ lib/IPC/Run/Win32IO.pm
@@ -31,7 +31,7 @@ require POSIX;
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.87';
+	$VERSION = '0.88';
 }
 
 use Socket qw( IPPROTO_TCP TCP_NODELAY );
