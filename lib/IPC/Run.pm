025c9e9b697fd79e81e431094576c1876a5ba919
Todd Rinaldo <toddr@cpan.org>
2012-08-22 14:29:00 +0200

    Bump to dev version (0.92_01) for a test release.

--- lib/IPC/Run.pm
+++ lib/IPC/Run.pm
@@ -1014,7 +1014,7 @@ use strict;
 use Exporter ();
 use vars qw{$VERSION @ISA @FILTER_IMP @FILTERS @API @EXPORT_OK %EXPORT_TAGS};
 BEGIN {
-	$VERSION = '0.91';
+	$VERSION = '0.92_01';
 	@ISA     = qw{ Exporter };
 
 	## We use @EXPORT for the end user's convenience: there's only one function
