7b474e3b8f79bae25ed4b5b8e87bac9e4dcd7d7b
Todd Rinaldo <toddr@cpan.org>
2018-03-26 16:34:48 -0500

    Bump version to 0.97 for release

--- lib/IPC/Run.pm
+++ lib/IPC/Run.pm
@@ -1015,7 +1015,7 @@ use Exporter ();
 use vars qw{$VERSION @ISA @FILTER_IMP @FILTERS @API @EXPORT_OK %EXPORT_TAGS};
 
 BEGIN {
-    $VERSION = '0.96';
+    $VERSION = '0.97';
     @ISA     = qw{ Exporter };
 
     ## We use @EXPORT for the end user's convenience: there's only one function
