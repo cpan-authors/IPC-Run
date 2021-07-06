cpan-authors/IPC-Run

commit fbd6d1823c325ad0b708b3c79ff078c93a386afa

Follow Windows argument quoting rules
Fixes https://github.com/toddr/IPC-Run/issues/142

Makefile.PL
@@ -16,8 +16,9 @@ if ( $^O ne 'MSWin32' ) {
     }
 }
 else {
-    $PREREQ_PM{'Win32::Process'} = '0.14';
-    $PREREQ_PM{'Win32API::File'} = '0.0901';
+    $PREREQ_PM{'Win32::Process'}    = '0.14';
+    $PREREQ_PM{'Win32::ShellQuote'} = 0;
+    $PREREQ_PM{'Win32API::File'}    = '0.0901';
     if ( $] >= 5.021006 ) {
         $PREREQ_PM{'Win32API::File'} = '0.1203';
     }

lib/IPC/Run/Win32Helper.pm
@@ -40,6 +40,7 @@ require POSIX;
 
 use Text::ParseWords;
 use Win32::Process;
+use Win32::ShellQuote ();
 use IPC::Run::Debug;
 use Win32API::File qw(
   FdGetOsFHandle
@@ -323,6 +324,11 @@ trying to be a little cross-platform here.  The only difference is
 that "\" is *not* treated as an escape except when it precedes 
 punctuation, since it's used all over the place in DOS path specs.
 
+TODO: strip caret escapes?
+
+TODO: use
+https://docs.microsoft.com/en-us/cpp/cpp/main-function-command-line-args#parsing-c-command-line-arguments
+
 TODO: globbing? probably not (it's unDOSish).
 
 TODO: shebang emulation? Probably, but perhaps that should be part
@@ -442,11 +448,7 @@ sub win32_spawn {
     }
 
     my $process;
-    my $cmd_line = join " ", map {
-        ( my $s = $_ ) =~ s/"/"""/g;
-        $s = qq{"$s"} if /[\"\s]|^$/;
-        $s;
-    } @$cmd;
+    my $cmd_line = Win32::ShellQuote::quote_native(@$cmd);
 
     _debug "cmd line: ", $cmd_line
       if _debugging;

t/run.t
@@ -38,7 +38,7 @@ sub get_warnings {
 select STDERR;
 select STDOUT;
 
-use Test::More tests => 268;
+use Test::More tests => 272;
 use IPC::Run::Debug qw( _map_fds );
 use IPC::Run qw( :filters :filter_imp start );
 
@@ -209,6 +209,31 @@ is $? >> 8, 42;
 
 is( _map_fds, $fd_map );
 $fd_map = _map_fds;
+
+##
+## Arguments bearing most bytes, excluding NUL (unsupported) and BEL (noisy and
+## not otherwise special).  Arguments bearing special sequences of bytes.
+##
+{
+    local $ENV{PERL_UNICODE};
+    delete $ENV{PERL_UNICODE};
+
+    my @bytes = map { $_ == 7 ? () : pack( 'C', $_ ); } 1 .. 0xFF;
+    $r = run(
+        [ $perl, '-e', 'binmode STDOUT; print join "\0", @ARGV', @bytes ],
+        '>', \$out
+    );
+    eok( $out, join "\0", @bytes );
+
+    my $sequences = qq{\\"\\az\\\\"\\\\\\};
+    foreach my $payload ( join( '', @bytes ), $sequences, "$sequences\n" ) {
+        $r = run(
+            [ $perl, '-e', 'binmode STDOUT; print @ARGV', $payload ],
+            '>', \$out
+        );
+        eok( $out, $payload );
+    }
+}
 
 ##
 ## A function

t/win32_compile.t
@@ -35,7 +35,8 @@ BEGIN {
         plan( skip_all => "android does not support getprotobyname()" );
     }
 
-    $INC{$_} = 1 for qw( Win32/Process.pm Win32API/File.pm );
+    $INC{$_} = 1 for qw(
+      Win32/Process.pm Win32/ShellQuote.pm Win32API/File.pm );
 
     package Win32API::File;
 

commit c299a86c9a292375fbfc39fb756883c80adac4b0

Support executing Win32 batch files.
Trivial examples worked, but my commit
fbd6d1823c325ad0b708b3c79ff078c93a386afa broke even those.  Cover all
known cases, subject to assumptions now described in documentation.  As
a surviving incompatibility compared to the last release, IPC::Run still
adds quotation marks to arguments where quotation marks are optional.
That is consequential in a batch file running "echo %*", for example.
One can argue that it's a good thing, but it is an incompatibility.

Discourage executing batch files with arguments, particularly when
argument values contain non-alphanumeric characters.  IPC::Run now
handles them well, but the batch file itself may not.

Makefile.PL
@@ -16,6 +16,7 @@ if ( $^O ne 'MSWin32' ) {
     }
 }
 else {
+    $PREREQ_PM{'Win32'}             = '0.27';     # for CSIDL_SYSTEM
     $PREREQ_PM{'Win32::Process'}    = '0.14';
     $PREREQ_PM{'Win32::ShellQuote'} = 0;
     $PREREQ_PM{'Win32API::File'}    = '0.0901';

lib/IPC/Run.pm
@@ -4162,6 +4162,34 @@ High resolution timeouts.
 
 =over
 
+=item argument-passing rules are program-specific
+
+Win32 programs receive all arguments in a single "command line" string.
+IPC::Run assembles this string so programs using L<standard command line parsing
+rules|https://docs.microsoft.com/en-us/cpp/cpp/main-function-command-line-args#parsing-c-command-line-arguments>
+will see an C<argv> that matches the array reference specifying the command.
+Some programs use different rules to parse their command line.  Notable examples
+include F<cmd.exe>, F<cscript.exe>, and Cygwin programs called from non-Cygwin
+programs.  Use L<Win32::Process>, not IPC::Run, to call these and other
+nonstandard programs.
+
+=item batch files
+
+Properly escaping a batch file argument depends on how the script will use that
+argument, because some uses experience multiple levels of caret (escape
+character) removal.  Avoid calling batch files with arguments, particularly when
+the argument values originate outside your program or contain non-alphanumeric
+characters.  Perl scripts and PowerShell scripts are sound alternatives.  If you
+do use batch file arguments, IPC::Run escapes them so the batch file can pass
+them, unquoted, to a program having standard command line parsing rules.  If the
+batch file enables delayed environment variable expansion, it must disable that
+feature before expanding its arguments.  For example, if F<foo.cmd> contains
+C<perl %*>, C<run ['foo.cmd', @list]> will create a Perl process in which
+C<@ARGV> matches C<@list>.  Prepending a C<setlocal enabledelayedexpansion> line
+would make the batch file malfunction, silently.  Another silent-malfunction
+example is C<run ['outer.bat', @list]> for F<outer.bat> containing C<foo.cmd
+%*>.
+
 =item Fails on Win9X
 
 If you want Win9X support, you'll have to debug it or fund me because I

lib/IPC/Run/Win32Helper.pm
@@ -38,7 +38,9 @@ BEGIN {
 
 require POSIX;
 
+use File::Spec ();
 use Text::ParseWords;
+use Win32 ();
 use Win32::Process;
 use Win32::ShellQuote ();
 use IPC::Run::Debug;
@@ -405,6 +407,67 @@ sub _dup2_gently {
 sub win32_spawn {
     my ( $cmd, $ops ) = @_;
 
+    my ( $app, $cmd_line );
+    my $need_pct = 0;
+    if ( $cmd->[0] !~ /\.(bat|cmd) *$/i ) {
+        $app      = $cmd->[0];
+        $cmd_line = Win32::ShellQuote::quote_native(@$cmd);
+    }
+    else {
+        # Batch file, so follow the batch-specific guidance of
+        # https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessa
+        # There's no one true way to locate cmd.exe.  In the unlikely event that
+        # %COMSPEC% is missing, fall back on a Windows API.  We could search
+        # %PATH% like _wsystem() does.  That would be prone to security bugs,
+        # and one fallback is enough.
+        $app = (
+            $ENV{COMSPEC}
+              || File::Spec->catfile(
+                Win32::GetFolderPath(Win32::CSIDL_SYSTEM),
+                'cmd.exe'
+              )
+        );
+
+        # Win32 rejects attempts to create files with names containing certain
+        # characters.  Ignore most, but reject the subset that might otherwise
+        # cause us to execute the wrong file instead of failing cleanly.
+        if ( $cmd->[0] =~ /["\r\n\0]/ ) {
+            croak "invalid batch file name";
+        }
+
+        # Make cmd.exe see the batch file name as quoted.  Suppose we instead
+        # used caret escapes, as we do for arguments.  cmd.exe could then "break
+        # the command token at the first occurrence of <space> , ; or ="
+        # (https://stackoverflow.com/a/4095133).
+        my @parts = qq{"$cmd->[0]"};
+
+        # cmd.exe will strip escapes once when parsing our $cmd_line and again
+        # where the batch file injects the argument via %*, %1, etc.  Compensate
+        # by adding one extra cmd_escape layer.
+        if ( @$cmd > 1 ) {
+            my @q = Win32::ShellQuote::quote_cmd( @{$cmd}[ 1 .. $#{$cmd} ] );
+            push @parts, map { Win32::ShellQuote::cmd_escape($_) } @q;
+        }
+
+        # One can't stop cmd.exe from expanding %var%, so inject each literal %
+        # via an environment variable.  Delete that variable before the real
+        # child can see it.  See
+        # https://www.dostips.com/forum/viewtopic.php?f=3&t=10131 for more on
+        # this technique and the limitations of alternatives.
+        $cmd_line = join ' ', @parts;
+        if ( $cmd_line =~ s/%/%ipcrunpct%/g ) {
+            $cmd_line = qq{/c "set "ipcrunpct=" & $cmd_line"};
+            $need_pct = 1;
+        }
+        else {
+            $cmd_line = qq{/c "$cmd_line"};
+        }
+    }
+    _debug "app: ", $app
+      if _debugging;
+    _debug "cmd line: ", $cmd_line
+      if _debugging;
+
     ## NOTE: The debug pipe write handle is passed to pump processes as STDOUT.
     ## and is not to the "real" child process, since they would not know
     ## what to do with it...unlike Unix, we have no code executing in the
@@ -447,15 +510,11 @@ sub win32_spawn {
         }
     }
 
+    local $ENV{ipcrunpct} = '%' if $need_pct;
     my $process;
-    my $cmd_line = Win32::ShellQuote::quote_native(@$cmd);
-
-    _debug "cmd line: ", $cmd_line
-      if _debugging;
-
     Win32::Process::Create(
         $process,
-        $cmd->[0],
+        $app,
         $cmd_line,
         1,    ## Inherit handles
         0,    ## Inherit parent priortiy class. Was NORMAL_PRIORITY_CLASS

t/run.t
@@ -38,7 +38,7 @@ sub get_warnings {
 select STDERR;
 select STDOUT;
 
-use Test::More tests => 272;
+use Test::More tests => 286;
 use IPC::Run::Debug qw( _map_fds );
 use IPC::Run qw( :filters :filter_imp start );
 
@@ -214,25 +214,134 @@ $fd_map = _map_fds;
 ## Arguments bearing most bytes, excluding NUL (unsupported) and BEL (noisy and
 ## not otherwise special).  Arguments bearing special sequences of bytes.
 ##
-{
+sub bytes_tests {
+    my ( $printer, $bytes, $sequences ) = @_;
+
     local $ENV{PERL_UNICODE};
     delete $ENV{PERL_UNICODE};
 
-    my @bytes = map { $_ == 7 ? () : pack( 'C', $_ ); } 1 .. 0xFF;
-    $r = run(
-        [ $perl, '-e', 'binmode STDOUT; print join "\0", @ARGV', @bytes ],
-        '>', \$out
-    );
-    eok( $out, join "\0", @bytes );
+    run( [ @$printer, @$bytes ], '>', \$out );
+    eok( $out, join "\0", @$bytes );
 
-    my $sequences = qq{\\"\\az\\\\"\\\\\\};
-    foreach my $payload ( join( '', @bytes ), $sequences, "$sequences\n" ) {
-        $r = run(
-            [ $perl, '-e', 'binmode STDOUT; print @ARGV', $payload ],
-            '>', \$out
-        );
+    foreach my $payload ( join( '', @$bytes ), @$sequences ) {
+        run( [ @$printer, $payload ], '>', \$out );
         eok( $out, $payload );
     }
+}
+
+my @bytes = map { $_ == 7 ? () : pack( 'C', $_ ); } 1 .. 0xFF;
+my $sequence = qq{\\"\\az\\\\"\\\\\\};
+bytes_tests(
+    [ $perl, '-e', 'binmode STDOUT; print join "\0", @ARGV' ],
+    \@bytes, [ $sequence, "$sequence\n" ]
+);
+
+##
+## Executing Cygwin Perl (from any Perl)
+##
+SKIP: {
+    # It's tempting to enable these automatically when c:/cygwin64/bin/perl
+    # exists.  However, a hostile user could create that file on a system having
+    # no Cygwin installation.
+    skip( 'set TEST_CYGWIN_PERL=c:/cygwin64/bin/perl to test executing Cygwin from non-Cygwin', 3 )
+      unless $ENV{TEST_CYGWIN_PERL};
+
+    # Cygwin mishandles arguments containing non-ASCII bytes, even under
+    # LANG=POSIX.  (It might handle them correctly if they're valid utf8 or if
+    # one installs a non-utf8 locale.)  Cygwin also mishandles "\\\\x a" and
+    # other lpCommandLine fragments with multiple backslashes.  (Cygwin programs
+    # executing other Cygwin programs pass the argv outside lpCommandLine,
+    # bypassing the problem.)  Don't test any of those.
+    my @cygbytes = map { $_ == 7 ? () : pack( 'C', $_ ); } 1 .. 0x7F;
+    bytes_tests(
+        [
+            $ENV{TEST_CYGWIN_PERL},
+            '-e', 'binmode STDOUT; print join "\0", @ARGV'
+        ],
+        \@cygbytes,
+        [$sequence]
+    );
+}
+
+##
+## Win32 batch files
+##
+SKIP: {
+    if ( !IPC::Run::Win32_MODE() ) {
+        skip( "batch files are specific to Win32", 11 );
+    }
+
+    use Cwd        ();
+    use File::Spec ();
+    use File::Temp ();
+    require Win32::ShellQuote;
+
+    my $parent_dir = File::Temp::tempdir( CLEANUP => 1 );
+    my $simple = File::Spec->catfile( $parent_dir, 'simple.bat' );
+    my $dir = File::Spec->catdir( $parent_dir, 'sub dir' );
+    mkdir $dir;
+
+    # List bytes that are valid in file names and potentially interesting to
+    # test.  Exclude the colon, which selects a non-default "file stream".
+    # Exclude ASCII alphanumeric bytes, which are uninteresting and would make
+    # the name too long.
+    my $file_name_bytes = join '', map {
+        my $chr = pack( 'C', $_ );
+        my $chr_file = File::Spec->catfile( $dir, "name_$chr.bat" );
+        $chr !~ /[:a-zA-Z0-9]/ && open( my $fh, '>', $chr_file ) ? ($chr) : ();
+    } 1 .. 0xFF;
+    my $bat          = File::Spec->catfile( $dir, 'SCRIPT FILE.BAT' );
+    my $cmd_basename = "SCRIPT ^&%SYSTEMROOT%!ipcrunpct!${file_name_bytes}.cmd  ";
+    my $cmd          = File::Spec->catfile( $dir, $cmd_basename );
+    my $bat_source   = sprintf(
+        qq{\@echo off\n"%s" -e %s %%*},
+        $perl,
+        Win32::ShellQuote::quote_cmd('binmode STDOUT; print join "\0", @ARGV')
+    );
+
+    # Fill batch files and check their handling of trivial arguments.
+    foreach my $f ( $simple, $bat, $cmd ) {
+        spit( $f, $bat_source );
+        run( [ $f, qw(xyz 1 23) ], '>', \$out );
+        eok( $out, join( "\0", qw(xyz 1 23) ) );
+    }
+
+    # Zero arguments
+    run( [$simple], '>', \$out );
+    eok( $out, '' );
+
+    # Forbidden character in batch file argument
+    eval { run( [ $simple, "foo\nbar" ], '>', \$out ); };
+    like( $@, qr/newline/ );
+
+    # Missing COMSPEC
+    {
+        local $ENV{COMSPEC};
+        run( [ $simple, 'arg' ], '>', \$out );
+        eok( $out, 'arg' );
+    }
+
+    # Environment variable collision
+    {
+        local $ENV{'somevar^^^'} = 'FAIL';
+        run( [ $simple, '%somevar%' ], '>', \$out );
+        eok( $out, '%somevar%' );
+    }
+
+    ## As above, except that cmd.exe does not cope with \r or \n.
+    bytes_tests(
+        [$cmd],
+        [ grep { $_ ne "\r" && $_ ne "\n" } @bytes ], [$sequence]
+    );
+
+    # Relative path doesn't begin with a volume specification, so it exercises
+    # different cmd.exe behavior.  See https://stackoverflow.com/a/4095133.
+    my $initial_cwd = Cwd::getcwd;
+    chdir $parent_dir;
+    my $rel = File::Spec->catfile( 'sub dir', $cmd_basename );
+    run( [ $rel, 'arg' ], '>', \$out );
+    eok( $out, 'arg' );
+    chdir $initial_cwd;
 }
 
 ##

t/win32_compile.t
@@ -36,7 +36,20 @@ BEGIN {
     }
 
     $INC{$_} = 1 for qw(
-      Win32/Process.pm Win32/ShellQuote.pm Win32API/File.pm );
+      Win32.pm Win32/Process.pm Win32/ShellQuote.pm Win32API/File.pm );
+
+    package Win32;
+
+    use vars qw( @ISA @EXPORT );
+
+    @ISA    = qw( Exporter );
+    @EXPORT = qw(
+      CSIDL_SYSTEM
+    );
+
+    eval "sub $_ {}" for @EXPORT;
+
+    use Exporter;
 
     package Win32API::File;
 