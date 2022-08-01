cpan-authors/IPC-Run

commit 18fa20efd64d928da21dfb4696a86a2db76b6efd

Avoid warning with IPCRUNDEBUG, in Windows spawned children.

lib/IPC/Run/Debug.pm
@@ -237,7 +237,7 @@ sub _debug {
       " ",
       join(
          "",
-         defined $IPC::Run::cur_self ? "#$IPC::Run::cur_self->{ID}" : (),
+         defined $IPC::Run::cur_self->{ID} ? "#$IPC::Run::cur_self->{ID}" : (),
          "($$)",
       ),
       defined $debug_name && length $debug_name ? $debug_name        : (),

commit 0458970b81c6970c3557e442fd0a09334301e4a3

Fix "Avoid warning with IPCRUNDEBUG, in Windows spawned children."
Commit 18fa20efd64d928da21dfb4696a86a2db76b6efd caused unwanted
autovivification on all platforms.  Testing t/pump.t under
IPCRUNDEBUG=details revealed the regression.

lib/IPC/Run/Debug.pm
@@ -237,7 +237,9 @@ sub _debug {
       " ",
       join(
          "",
-         defined $IPC::Run::cur_self->{ID} ? "#$IPC::Run::cur_self->{ID}" : (),
+         defined $IPC::Run::cur_self && defined $IPC::Run::cur_self->{ID}
+         ? "#$IPC::Run::cur_self->{ID}"
+         : (),
          "($$)",
       ),
       defined $debug_name && length $debug_name ? $debug_name        : (),