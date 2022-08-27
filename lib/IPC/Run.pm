8260ef756da8586a420e17d70366fb05cd7407c6
Máté Szabó <szmate1618@gmail.com>
2022-08-27 23:12:44 +0200

    croak on undef in harness subroutine

--- lib/IPC/Run.pm
+++ lib/IPC/Run.pm
@@ -1803,6 +1803,8 @@ sub harness {
                     croak "Process control symbol ('|', '&') missing" if $cur_kid;
                     croak "Can't spawn a subroutine on Win32"
                       if Win32_MODE && ref eq "CODE";
+                    croak "Can't run undefined command. Did you pass a reference to an undefined array?"
+                      if !defined($_->[0]) && ref eq 'ARRAY';
                     $cur_kid = {
                         TYPE   => 'cmd',
                         VAL    => $_,
